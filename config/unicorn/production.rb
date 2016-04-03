370e0af015e207f11759c6274d6af3152d77689b
Stanislav Gernyak <stager94@gmail.com>
2016-04-03 17:31:34 +0300

    -

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,4 +1,4 @@
-deploy_to  = "/var/www/analyzis"
+deploy_to  = "/var/www/pzp"
 rails_root = "#{deploy_to}/current"
 pid_file   = "#{deploy_to}/shared/pids/unicorn.pid"
 socket_file= "#{deploy_to}/shared/unicorn.sock"
